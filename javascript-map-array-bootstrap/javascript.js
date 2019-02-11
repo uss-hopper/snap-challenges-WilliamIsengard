e471f554776af5d002af17f3eef799a7ea65d6fd
William Isengard <william.isengard@gmail.com>
2019-02-11 09:54:47 -0700

    Updated js function to include createElement and appendChild functions

--- javascript-map-array-bootstrap/javascript.js
+++ javascript-map-array-bootstrap/javascript.js
@@ -2,30 +2,12 @@
 
 const array = [{"userId":1,"id":1,"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body":"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"},{"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"},{"userId":1,"id":3,"title":"ea molestias quasi exercitationem repellat qui ipsa sit aut","body":"et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"},{"userId":1,"id":4,"title":"eum et est occaecati","body":"ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"},{"userId":1,"id":5,"title":"nesciunt quas odio","body":"repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"}];
 
-const title1 = array.map(index => `${index.title}`);
+let titles = array.map(function(element) {return element.title;});
 
-let element1 = title[0];
+let title1 = document.createElement('span');
 
-var element1 = Document.getElementById("cardTitle1");
-var bodyElement = document.body;
-bodyElement.innerHTML = element1 + bodyElement.innerHTML;
+title1.innerText = titles[0];
 
-// div.innerHTML = title[0];
+document.getElementById('cardTitle1').appendChild(objectOne);
 
-// var element = document.getElementById("one");
-// var newElement = '<div id="two">two</div>'
-// element.insertAdjacentHTML( 'afterend', newElement )
-
-// Document.createElement(cardTitle1);
-//
-//
-//
-// const body = array.map(index => `${index.body}`);
-//
-// console.log(body);
-//
-//
-//
-// let newArray = Document.getElementById("card");
-//
-// Document.createElement()
+let titles = array.map(function(element) {return element.title;});
