a450dde89fd8ad92cf47b8a8c3fdce5dad3e7976
William Isengard <william.isengard@gmail.com>
2019-02-18 08:43:45 -0700

    Created functions using reduce to sum and multiply array

---
+++ javascriptReduceMultiple.js
@@ -0,0 +1,11 @@
+//using reduce multiple the array
+
+//sum
+const numbers = [1,2,3,5,8,13,21,34];
+const sum = numbers.reduce((total, amount) => total + amount);
+sum;
+
+//multiply
+const numbers = [1,2,3,5,8,13,21,34];
+const multiply = numbers.reduce((total, amount) => total * amount);
+multiply;
