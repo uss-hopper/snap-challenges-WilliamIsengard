996a617c6a2393a9034d036828d00fc8654ee0d6
William Isengard <william.isengard@gmail.com>
2019-02-21 08:58:03 -0700

    Added alternative formatting of reduce function

--- snap-reverse/script.js
+++ snap-reverse/script.js
@@ -6,6 +6,13 @@ var arr = "I'll alert the crew. We finished our first sensor sweep of the neutra
 var split = arr.split(" ");
 split.reduce((a, b) => [b].concat(a), []);
 
+//** Alternative Formatting **//
+let string = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
+
+let newString = string.split(""); //No Space Between Quotes Reverses Letters Within Words
+let finalString = newString.reduce((accumulator, current) => current + accumulator);
+console.log(finalString);
+
 //** Reverse Via Reverse Method **//
 var arr = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
 
