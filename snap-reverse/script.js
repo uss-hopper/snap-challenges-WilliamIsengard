4367725b2bf4246ce24a2e58aa86db91a94f2f0e
William Isengard <william.isengard@gmail.com>
2019-02-21 08:36:47 -0700

    Cleaned up code layout

--- snap-reverse/script.js
+++ snap-reverse/script.js
@@ -1,14 +1,12 @@
-// snap-reverse//
+//**** snap-reverse ****//
 
-//Reverse Via Reduce Method//
+//** Reverse Via Reduce Method **//
 var arr = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
 
 var split = arr.split(" ");
+split.reduce((a, b) => [b].concat(a), []);
 
-split.reduce((a, b) => [b].concat(a), [])
-
-
-//Reverse Via Reverse Method//
+//** Reverse Via Reverse Method **//
 var arr = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
 
 var split = arr.split(" ");
