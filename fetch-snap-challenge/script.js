e721923e991acdf3cd72f3125030a6a927b39543
William Isengard <william.isengard@gmail.com>
2019-03-14 09:19:13 -0600

    Corrected typo. Button click displays [object Promise] on dom. More work needed.

--- fetch-snap-challenge/script.js
+++ fetch-snap-challenge/script.js
@@ -1,4 +1,4 @@
-// using fetch display posts from jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
+// using fetch display posts fro jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
 // https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
