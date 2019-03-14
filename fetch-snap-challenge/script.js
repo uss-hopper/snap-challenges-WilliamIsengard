fd3a4d41e28774685d416daa9f786c3df1ec7334
William Isengard <william.isengard@gmail.com>
2019-03-14 09:16:46 -0600

    Made adjustments to JS, but function not working.

--- fetch-snap-challenge/script.js
+++ fetch-snap-challenge/script.js
@@ -1,17 +1,13 @@
-// using fetch display posts fro jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
+// using fetch display posts from jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
 // https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
 function button1() {
-	window.onclick = document.getElementById("paragraph1").innerHTML='json';
-
-var json = fetch('http://bar.com/data.json', {
-  mode: 'no-cors'
-})
-.then(function() {
-  JSON.parse(json)
-})
-.catch(function(error) {
-  console.log('Something went wrong: \n', error);
-});
-
+	window.onclick = document.getElementById("paragraph1").innerHTML = fetch('http://bar.com/data.json')
+		.then(function(response) {
+			JSON.parse(response)
+		})
+		.catch(function(error) {
+			console.log('Something went wrong: \n', error);
+		});
+}
