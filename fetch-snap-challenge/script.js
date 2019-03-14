42638d8ffb82281017fd0c146255dc1c67dec96a
William Isengard <william.isengard@gmail.com>
2019-03-14 10:02:56 -0600

    Added working solution to snap challenge.

--- fetch-snap-challenge/script.js
+++ fetch-snap-challenge/script.js
@@ -1,13 +1,38 @@
 // using fetch display posts fro jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
 // https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+//
+// function button1() {
+// 	window.onclick = document.getElementById("paragraph1").innerHTML = fetch('https://jsonplaceholder.typicode.com/posts')
+// 		.then((response) => {
+// 			return response.json();
+// 		})
+// 		.catch(function(error) {
+// 			console.log('Something went wrong: \n', error);
+// 		});
+// }
 
-function button1() {
-	window.onclick = document.getElementById("paragraph1").innerHTML = fetch('http://bar.com/data.json')
-		.then(function(response) {
-			JSON.parse(response)
+//Correct solution//
+
+function loadPosts() {
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then((response) => {
+			console.log(response)
+			return response.json();
 		})
-		.catch(function(error) {
-			console.log('Something went wrong: \n', error);
+		.then((myJson) => {
+			const cards = myJson.map(index => {
+				return `
+  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">${index.title}</h5>
+    <p class="card-text">${index.body}</p>
+  </div>
+  </div>
+    `
+			});
+			let html = document.getElementById("target")
+			html.innerHTML = cards.join("\r\n");
 		});
+
 }
